ed745d74d605d4cde353a3d17e9f5b3f01d1edbe
Junqiu Zhu <2434078451@qq.com>
2026-07-24 20:12:32 -0700

    1
    1

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,38 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-CgFrDGAk.js\")",
+  "clientReferenceDeps": {
+    "ffa4af8fdc37": {
+      "js": [
+        "/assets/index-CgFrDGAk.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "e791668edc18": {
+      "js": [
+        "/assets/layout-segment-context-xMBwpQnh.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-CgFrDGAk.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "b843a7408a43": {
+      "js": [
+        "/assets/index-CgFrDGAk.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-9D8WYrE2.css"
+      ]
+    }
+  }
+}
